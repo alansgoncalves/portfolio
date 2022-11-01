7a2de8fd2acf6cdfcf319f26b82c60d21786b7ef
alansgoncalves <vekio.soft@gmail.com>
2022-11-01 12:29:50 -0300

    feat: create home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Olá, sou</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Alan Gonçalves
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          Desenvolvedor Full Stack.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          Sou pós-graduado em Administração de Empresas pela FGV com
+          especialização em Desenvolvimento Web Fullstack pela Trybe e vivência
+          em grandes projetos de desenvolvimento web e implantação de sistemas
+          ERP's. Passagem por empresas como Unilever, GlaxoSmithKline e Portal
+          iG, onde desenvolvi excelentes habilidades de soft skills e gestão de
+          processos.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            Projetos
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
